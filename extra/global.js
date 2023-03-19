2d39990857029921f554461b5d71bea6d0b676bf
roshan <roshankumarsingh9876@gmail.com>
2023-03-19 22:01:19 +0530

    first commit

---
+++ extra/global.js
@@ -0,0 +1,11 @@
+
+//console.log(global);
+// global.setTimeout(()=>{//global object methode
+//     console.log('in the timeout');
+//     clearInterval(int);
+// },3000);
+
+// const int=setInterval(()=>{
+// console.log('in the interval');
+// },1000)
+
---
+++ extra/people.js
@@ -0,0 +1,4 @@
+const people = ['roshan','vikash','shivam','shubham','sukla'];
+const ages=[20,25,30,35];
+console.log(people);
+module.exports = {people,ages};
